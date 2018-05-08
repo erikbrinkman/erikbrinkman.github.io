0a9281cd75809450973016b60f573c4495a494c3
Erik Brinkman <erik.brinkman@gmail.com>
2018-05-08 13:37:30 -0400

    Update google analytics

--- script.js
+++ script.js
@@ -85,20 +85,4 @@
 
   // Trigger initial scroll event for coloring
   window.dispatchEvent(new Event('scroll'));
-
-  // Google analytics
-  (function (b, o, i, l, e, r) {
-    b.GoogleAnalyticsObject = l;
-    b[l] || (b[l] =
-        function () {
-          (b[l].q = b[l].q || []).push(arguments)
-        });
-    b[l].l = +new Date;
-    e = o.createElement(i);
-    r = o.getElementsByTagName(i)[0];
-    e.src = 'https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e, r)
-  }(window, document, 'script', 'ga'));
-  ga('create', 'UA-37133085-2');
-  ga('send', 'pageview');
 })();
